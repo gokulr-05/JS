48288019e0d7c06d82fcd195a304b2063a7cf894
gokulr-05 <gokulravivcr05@gmail.com>
2022-07-19 13:20:37 +0530

    execution context & variable environment

---
+++ script.js
@@ -0,0 +1,45 @@
+// document.getElementById("btn1").addEventListener("click", () => {
+//   console.log("btn clicked");
+// });
+
+// function a() {
+//   {
+//     var a1 = 10;
+//   }
+
+//   function b() {
+//     console.log(a1);
+//   }
+
+//   b();
+// }
+
+// a();
+
+let a = 10;
+var b = 20;
+
+function func1() {
+  let func1_a = 30;
+  var func1_b = 40;
+}
+
+let func2 = function () {
+  let func2_a = 50;
+  var func2_b = 60;
+};
+
+let func3 = () => {
+  let func3_a = 70;
+  var func3_b = 80;
+};
+
+var func4 = function () {
+  let func2_a = 50;
+  var func2_b = 60;
+};
+
+var func5 = () => {
+  let func3_a = 70;
+  var func3_b = 80;
+};
